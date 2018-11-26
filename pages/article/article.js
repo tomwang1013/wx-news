bc2451a6dcd755d5960b4d4b0c4863c5ffcb26e6
tomwang1013 <174604588@qq.com>
2018-11-26 19:46:47 +0800

    fix review

--- pages/article/article.js
+++ pages/article/article.js
@@ -43,13 +43,6 @@ Page({
     this.getDetail();
   },
 
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-    this.getDetail(() => wx.stopPullDownRefresh());
-  },
-
   goBack() {
     wx.navigateBack({
       delta: 1
